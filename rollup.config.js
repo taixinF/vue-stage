038dc3cf2e168dc59754467d40f6301e897761e2
taixinF <yclzbb1349458861@163.com>
2022-09-01 12:07:25 +0800

    初始化

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+// rollup 默认到处一个对象 作为打包的配置文件
+import babel from "rollup-plugin-babel";
+
+export default {
+  input: "./src/index.js", //入口
+  output: {
+    file: "./dist/vue.js", //出口
+    //new Vue
+    name: "Vue", //global.vue
+    format: "umd", //esm es6模块 commonjs模块  iife自执行函数 umd
+    sourcemap: true, //希望可以调试源代码
+  },
+  plugins: [
+    babel({
+      exclude: "node_modules/**",
+    }),
+  ],
+};
+
+//为什么vue2 只能支持ie9 以上 Object.defineProperty  不支持低版本的
+// proxy是es6 的也没有替代方案
---
+++ src/index.js
@@ -0,0 +1,5 @@
+export const a = 11110;
+
+debugger;
+
+export default { a: 1 };
