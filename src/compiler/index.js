1abb5e8138d5e675801bc1d1b3adb4239a713020
taixinF <yclzbb1349458861@163.com>
2022-09-03 00:30:33 +0800

    模板转化ast语法树正则

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -1,6 +1,17 @@
+const ncname = `[a-zA-Z_][\\-\\.0-9_a-zA-Z]*`;
+const qnameCapture = `((?:${ncname}\\:)?${ncname})`;
+const startTagOpen = new RegExp(`^<${qnameCapture}>`);
+const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`);
+const attribute =
+  /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/;
+const startTagClose = /^\s*(\/?)>/;
+const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/g;
+
 //对模板进行编译处理
 export function compileToFuncrion(template) {
   // 1 就是将template 转化成ast语法树
   // 2 生成render 方法 render 方法执行执行后的返回结果·就是虚拟DOm
+
+  
   console.log(template);
 }
