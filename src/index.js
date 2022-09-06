fd4e90a4ee9ad5234ec0d06e2ccb912f6488903c
taixinF <yclzbb1349458861@163.com>
2022-09-06 20:02:26 +0800

    实现异步更新原理

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import { initMixin } from "./init";
 import { initLifeCycle } from "./lifecycle";
+import { nextTick } from "./observe/wacher";
 
 //将所有的方法都耦合在一起
 function Vue(options) {
   this._init(options); //默认就调用了init
 }
+Vue.prototype.$nextTick = nextTick;
 initMixin(Vue); //扩展了 init方法
-initLifeCycle(Vue)
+initLifeCycle(Vue);
+
 export default Vue;
--- src/observe/wacher.js
+++ src/observe/wacher.js
@@ -29,8 +29,81 @@ class Watcher {
     Dep.target = null;
   }
   updata() {
-    this.get(); //重新渲染
+    //缓存更新
+    queueWatcher(this); //把当前的watcher暂存起来
+    // this.get(); //重新渲染    //多次渲染
+  }
+  run() {
+    this.get(); //缓存渲染
   }
 }
+let queue = [];
+let has = {};
+let pending = false; //防抖
+function flushSchedulerQueue() {
+  let flushQueue = queue.slice(0);
+  queue = [];
+  has = {};
+  pending = false;
+  flushQueue.forEach((q) => q.run()); //刷新过沉重可能还有新的wathcer 放到queue中
+}
+function queueWatcher(wathcer) {
+  const id = wathcer.id;
+  if (!has[id]) {
+    queue.push(wathcer);
+    has[id] = true;
+    if (!pending) {
+      setTimeout(flushSchedulerQueue, 0);
+      pending = true;
+    }
+  }
+}
+//Vue核心异步批处理
 
+let callback = [];
+let waiting = false;
+function flushCallbacks() {
+  let cbs = callback.slice(0);
+  waiting = false;
+  callback = [];
+  cbs.forEach((cb) => cb()); //按照顺序依次执行
+}
+
+//nextTick中没有直接使用某个api 而是采用优雅降级的方式
+//内部先采用的是promise ie不兼容 mutationObserver(h5的api) 可以考虑ie专享 setImmediate setTimeout
+
+//nextTick 是同步还是异步
+//放到队列中是同步 开启定时器是异步
+
+let timerFunc;
+// if (Promise) {
+//   timerFunc = () => {
+//     Promise.resolve().then(flushCallbacks);
+//   };
+// } else if (MutationObserver) {
+//   let observer = new MutationObserver(flushCallbacks); //这里传入的回调是异步执行的
+//   let textNode = document.createTextNode(1);
+//   observer.observe(textNode, {
+//     characterData: true,
+//   });
+//   timerFunc = () => {
+//     textNode.textContent = 2;
+//   };
+// } else if (setImmediate) {
+//   timerFunc = () => {
+//     setImmediate(flushCallbacks);
+//   };
+// } else {
+timerFunc = () => {
+  setTimeout(flushCallbacks);
+};
+// }
+
+export function nextTick(cb) {
+  callback.push(cb); //维护nextTick中的callback方法
+  if (!waiting) {
+    timerFunc();
+    waiting = true;
+  }
+}
 export default Watcher;
